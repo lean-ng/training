ab71b1875133ac7abeccd50ef634db010ee41871
Micha Buchholz <mail@lean-stack.rocks>
2023-12-14 08:23:17 +0100

    feat: Add module for the submissions seed.

---
+++ samples/upvote/src/seed.ts
@@ -0,0 +1,38 @@
+export const submissions = [
+  {
+    id: 1,
+    title: 'Yellow Pail',
+    description: 'On-demand sand castle construction expertise.',
+    url: '#',
+    votes: 16,
+    avatar: '/assets/avatars/daniel.jpg',
+    submissionImage: '/assets/submissions/image-yellow.png',
+  },
+  {
+    id: 2,
+    title: 'Supermajority: The Fantasy Congress League',
+    description: 'Earn points when your favorite politicians pass legislation.',
+    url: '#',
+    votes: 11,
+    avatar: '/assets/avatars/kristy.png',
+    submissionImage: '/assets/submissions/image-rose.png',
+  },
+  {
+    id: 3,
+    title: 'Tinfoild: Tailored tinfoil hats',
+    description: 'We have your measurements and shipping address.',
+    url: '#',
+    votes: 17,
+    avatar: '/assets/avatars/veronika.jpg',
+    submissionImage: '/assets/submissions/image-steel.png',
+  },
+  {
+    id: 4,
+    title: 'Haught or Naught',
+    description: 'High-minded or absent-minded? You decide.',
+    url: '#',
+    votes: 9,
+    avatar: '/assets/avatars/molly.png',
+    submissionImage: '/assets/submissions/image-aqua.png',
+  },
+];
