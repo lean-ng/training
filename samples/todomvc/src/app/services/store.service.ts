3adc268e05bab36e90211ab9611c08b1ea8f5ac6
Micha Buchholz <mail@lean-stack.rocks>
2023-12-17 23:12:32 +0100

    feat: Make service mutable. For Simplicity.

--- samples/todomvc/src/app/services/store.service.ts
+++ samples/todomvc/src/app/services/store.service.ts
@@ -14,22 +14,37 @@ export class StoreService {
 
   async createTodo(title: string) {
     const todo = await this.persistence.createTodo(title);
-    this.todos = [...this.todos, todo];
+    // this.todos = [...this.todos, todo];
+
+    // Mutate for simplicity
+    this.todos.push(todo);
   }
 
   async updateTodoTitle(id: Todo['id'], title: string) {
     const todo = await this.persistence.updateTodo(id, { title });
-    this.todos = this.todos.map((t) => (t.id === id ? todo : t));
+    // this.todos = this.todos.map((t) => (t.id === id ? todo : t));
+
+    // Mutate for simplicity
+    const todoToUpdate = this.todos.find((t) => t.id === id);
+    todoToUpdate!.title = title;
   }
 
   async updateTodoCompletedState(id: Todo['id'], completed: boolean) {
     const todo = await this.persistence.updateTodo(id, { completed });
-    this.todos = this.todos.map((t) => (t.id === id ? todo : t));
+    // this.todos = this.todos.map((t) => (t.id === id ? todo : t));
+
+    // Mutate for simplicity
+    const todoToUpdate = this.todos.find((t) => t.id === id);
+    todoToUpdate!.completed = completed;
   }
 
   async deleteTodo(id: Todo['id']) {
     await this.persistence.deleteTodo(id);
-    this.todos = this.todos.filter((t) => t.id !== id);
+    // this.todos = this.todos.filter((t) => t.id !== id);
+
+    // Mutate for simplicity
+    const indexToDelete = this.todos.findIndex((t) => t.id === id);
+    this.todos.splice(indexToDelete, 1);
   }
 
   async deleteCompletedTodos() {
