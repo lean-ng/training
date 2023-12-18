028dc59e32a19a3750df667bb1597e1bf470b82e
Micha Buchholz <mail@lean-stack.rocks>
2023-12-18 04:48:33 +0100

    feat: Revert service and use best practices,  signals and control flow..

--- samples/todomvc/src/app/services/store.service.ts
+++ samples/todomvc/src/app/services/store.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { Todo } from '../model/todo';
 import { LocalPersistenceService } from './local-persistence.service';
 
@@ -6,56 +6,42 @@ import { LocalPersistenceService } from './local-persistence.service';
   providedIn: 'root',
 })
 export class StoreService {
-  todos: Todo[] = [];
+  private _todos = signal([] as Todo[]);
+  todos = this._todos.asReadonly();
 
   constructor(private persistence: LocalPersistenceService) {
-    persistence.getAllTodos().then((todos) => (this.todos = todos));
+    persistence.getAllTodos().then(todos => this._todos.set(todos));
   }
 
   async createTodo(title: string) {
     const todo = await this.persistence.createTodo(title);
-    // this.todos = [...this.todos, todo];
-
-    // Mutate for simplicity
-    this.todos.push(todo);
+    this._todos.update(todos => [...todos, todo]);
   }
 
   async updateTodoTitle(id: Todo['id'], title: string) {
     const todo = await this.persistence.updateTodo(id, { title });
-    // this.todos = this.todos.map((t) => (t.id === id ? todo : t));
-
-    // Mutate for simplicity
-    const todoToUpdate = this.todos.find((t) => t.id === id);
-    todoToUpdate!.title = title;
+    this._todos.update(todos => todos.map((t) => (t.id === id ? todo : t)));
   }
 
   async updateTodoCompletedState(id: Todo['id'], completed: boolean) {
     const todo = await this.persistence.updateTodo(id, { completed });
-    // this.todos = this.todos.map((t) => (t.id === id ? todo : t));
-
-    // Mutate for simplicity
-    const todoToUpdate = this.todos.find((t) => t.id === id);
-    todoToUpdate!.completed = completed;
+    this._todos.update(todos => todos.map((t) => (t.id === id ? todo : t)));
   }
 
   async deleteTodo(id: Todo['id']) {
     await this.persistence.deleteTodo(id);
-    // this.todos = this.todos.filter((t) => t.id !== id);
-
-    // Mutate for simplicity
-    const indexToDelete = this.todos.findIndex((t) => t.id === id);
-    this.todos.splice(indexToDelete, 1);
+    this._todos.update(todos => todos.filter((t) => t.id !== id));
   }
 
   async deleteCompletedTodos() {
     await Promise.all(
-      this.todos.filter((t) => t.completed).map((t) => this.deleteTodo(t.id))
+      this.todos().filter((t) => t.completed).map((t) => this.deleteTodo(t.id))
     );
   }
 
   async setAllTodosCompletedState(completed: boolean) {
     await Promise.all(
-      this.todos
+      this.todos()
         .filter((t) => t.completed !== completed)
         .map((t) => this.updateTodoCompletedState(t.id, completed))
     );
