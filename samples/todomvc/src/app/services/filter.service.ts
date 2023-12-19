93b668a1cdd81fe719fc032dd20d76aba159d093
Micha Buchholz <mail@lean-stack.rocks>
2023-12-19 21:31:20 +0100

    build: Configure formatting and format code.

--- samples/todomvc/src/app/services/filter.service.ts
+++ samples/todomvc/src/app/services/filter.service.ts
@@ -3,10 +3,9 @@ import { Injectable, signal } from '@angular/core';
 type Visibility = 'all' | 'active' | 'completed';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilterService {
-
   private _filter = signal<Visibility>('all');
   filter = this._filter.asReadonly();
 
