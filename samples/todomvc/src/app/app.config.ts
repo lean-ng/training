9c68dd4241755f656e4f10b9f562b6992e61a91a
Micha Buchholz <mail@lean-stack.rocks>
2023-12-19 21:28:25 +0100

    fix: Add linting and streamline code (previous commit 172ff35fe1c0794e9f35016063073a0354dfe7cf ).

--- samples/todomvc/src/app/app.config.ts
+++ samples/todomvc/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { TodosShellComponent } from './components/todos-shell/todos-shell.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter([
-    { path: '', component: TodosShellComponent },
-    { path: ':filter', component: TodosShellComponent }
-  ], withHashLocation())]
+  providers: [
+    provideRouter(
+      [
+        { path: '', component: TodosShellComponent },
+        { path: ':filter', component: TodosShellComponent },
+      ],
+      withHashLocation()
+    ),
+  ],
 };
